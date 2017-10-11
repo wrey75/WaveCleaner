d9ed41e69cc2a87dd6c61ef239f88ac1350e852f
William Rey <wrey75@gmail.com>
2017-10-11 08:23:36 +0200

    Unsaved parts

--- src/main/java/com/oxande/wavecleaner/filters/AudioFilter.java
+++ src/main/java/com/oxande/wavecleaner/filters/AudioFilter.java
@@ -2,6 +2,16 @@ package com.oxande.wavecleaner.filters;
 
 import ddf.minim.UGen;
 
+/**
+ * This is the base for audio filters. Note the audio
+ * filter is an high-end filter (mainly because it can
+ * work on many samples and not only one) but also because
+ * it can (and must drive) components to modify its
+ * working capabilities. 
+ * 
+ * @author wrey75
+ *
+ */
 public abstract class AudioFilter extends UGen {
 
 }
